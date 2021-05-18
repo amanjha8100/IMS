55ab39796f8d282c60fd4c5026bb7daee83d0d10
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-18 15:27:39 +0530

    dashboard design going on

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
 
+def index(request):
+    return render(request,'index.html')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index,name="index"),
 ]
