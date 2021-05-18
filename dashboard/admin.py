9efbad975e07fcdd78a8bc6452277f07cfa0f61d
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-19 04:14:37 +0530

    models added

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Product,Order
 # Register your models here.
+admin.site.site_header = 'Inventory Admin'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','category','quantity')
+    list_filter = ['category']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+cat_choices = (
+    ("Stationary","Stationary"),
+    ("Electronics","Electronics"),
+    ("Food","Food"),
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    category = models.CharField(max_length=100, choices=cat_choices,null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
+    class Meta:
+        verbose_name_plural = 'Product'
+        ordering = ['category']
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    staff = models.ForeignKey(User,models.CASCADE,null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Order'
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
