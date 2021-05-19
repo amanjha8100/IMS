1e2030c27ccead64d41cf6bf638b982df006a91c
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-19 21:47:56 +0530

    login,register and logout completed

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.db import models
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=150)
-    password1 = forms.CharField(forms.PasswordInput())
-    # password2 = forms.CharField
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm,self).__init__(*args, **kwargs)
+        self.fields['username'].widget = forms.TextInput(attrs={'type':'text', 'placeholder':'Username', 'id':'username', 'name':'username', 'class' : 'form-control'})
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'type':'password', 'placeholder':'Password....', 'id':'password1', 'name':'password1', 'class' : 'form-control'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'type':'password', 'placeholder':'Confirm Password.....', 'id':'password2', 'name':'password2', 'class' : 'form-control'})
+
--- users/views.py
+++ users/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from . forms import SignUpForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
-    return render(request,'users/register.html',{'form':form})
+    form = SignUpForm()
+    context = {
+        'form':form
+    }
+    
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            return render(request,"users/register.html",{'form':form})
+    return render(request,'users/register.html',context)
+
+def loginuser(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.info(request,'Username or Password is incorrect')
+    return render(request,"users/login.html")
+
+def logoutuser(request):
+    logout(request)
+    return redirect('login')
