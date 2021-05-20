b38f8a34ae6f93fcbedb9ff65284b10a9242278e
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-21 04:08:42 +0530

    bug fixing

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -9,4 +9,4 @@ class AddProduct(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(AddProduct,self).__init__(*args, **kwargs)
         self.fields['name'].widget = forms.TextInput(attrs={'type':'text','id':'name','name':'name','class':'form-control','placeholder':'Product Name'})
-        self.fields['category'].widget = forms.TextInput(attrs={'type':'choices','id':'category','name':'category','placeholder':'.........','class':'form-control'})
+        self.fields['category'].widget = forms.TextInput(attrs={'class':'form-control'})
