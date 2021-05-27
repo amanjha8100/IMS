2c1c06c5ae7582788233026760e4d9a9cff884a8
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-28 02:46:41 +0530

    reset password done

--- core/settings.py
+++ core/settings.py
@@ -133,3 +133,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#password reset
+EMAIl_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'demo@gmail.com'
+EMAIL_HOST_PASSWORD = 'passwords'
--- core/urls.py
+++ core/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     # path('profile/',profile,name="profile"),
     path('password_reset/',auth_views.PasswordResetView.as_view(),name="password_reset"),
     path('password_reset_done/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
-    path('password_reset_confirm/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('password_reset_complete/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
